e1fb19c39aabb257d588f0a923884bcc4a4ca149
Yayson Olivares <44871312+madeusblack@users.noreply.github.com>
2020-03-24 00:33:55 -0300

    Added index to book obj and added change read status button
    Added index to book obj and added change read status button
    todo:
    set firebase for online persistent data

--- app.js
+++ app.js
@@ -1,84 +1,97 @@
 
-  let myLibrary = [];
-  function Book(title, author, pages, read) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.read = read
+let myLibrary = [];
+lib = localStorage.getItem('datos');
+if(lib!=null){
+  myLibrary=JSON.parse(lib)
+}
+render()
+function Book(title, author, pages, read,objindex) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  this.objindex=objindex;
   }
-  Book.prototype.info = function() {
-    let toret= this.title +" by "+ this.author + ", " + this.pages + " pages," + this.read
-    return toret
-  }
-
 function addBookToLibrary() {
-   let newTitle=document.getElementById("bookname").value ;
-   let newAuthor=document.getElementById("author").value;
-   let newPages=document.getElementById("pages").value;
-   let newIsread=document.getElementById("read").checked;
-   console.log(newTitle,newAuthor,newPages,newIsread)
-    let book = new Book(newTitle,newAuthor,newPages,newIsread)
-    myLibrary.push(book)
-    render()
+  let newTitle=document.getElementById("bookname").value ;
+  let newAuthor=document.getElementById("author").value;
+  let newPages=document.getElementById("pages").value;
+  let newIsread=document.getElementById("read").checked;
+  let book = new Book(newTitle,newAuthor,newPages,newIsread)
+  myLibrary.push(book)
+  render()
 }
 function render(){
-  var list = document.getElementById("librarycont").hasChildNodes();
-  if (list==true){
-    console.log("rchild")
+  var checkNodes = document.getElementById("librarycont")
+  .hasChildNodes();
+  if (checkNodes==true){
     let element = document.getElementById("librarycont");
     while (element.firstChild) {
       element.removeChild(element.firstChild);
     }
   }
-  
   for (index = 0; index < myLibrary.length; index++) { 
+    myLibrary[index].objindex=index
     display(myLibrary[index],index); 
-} 
-        
-        
-   
-
+  } 
+  localStorage.setItem('datos', JSON.stringify(myLibrary));
 
 }
 function display(objct,indexofobj){
-    contain = document.querySelector('#librarycont')
-    let card = document.createElement('div');
-    card.classList.add('card1');
-    let titledis = document.createElement('h4');
-    let authdis=document.createElement('h4');
-    let pagesdis=document.createElement('h4');
-    let readdis=document.createElement('h4');
-    let delbutton=document.createElement('button');
-    let idobj=document.createElement('h6')
-    delbutton.classList.add('fa-close')
-    delbutton.classList.add('fa')
-    delbutton.classList.add('delbutton')
+  contain = document.querySelector('#librarycont')
+  let card = document.createElement('div');
+  card.classList.add('card1');
+  let titledis = document.createElement('h4');
+  let authdis=document.createElement('h4');
+  let pagesdis=document.createElement('h4');
+  let readdis=document.createElement('h4');
+  let idobj=document.createElement('h6')
+  let delbutton=document.createElement('button');
+  delbutton.classList.add('fa-close')
+  delbutton.classList.add('fa')
+  delbutton.classList.add('delbutton')
+  delbutton.addEventListener("click", function(){
+    deleteBook(indexofobj);}, false);
+    let chngbutton=document.createElement('button');
+    chngbutton.classList.add('fa')
+    chngbutton.classList.add('chngbutton')
+    chngbutton.addEventListener("click", function(){
+      changestate(objct);}, false);
 
-    delbutton.addEventListener("click", function(){
-      testfunc(indexofobj);
-  }, false);
-    idobj.innerHTML="ID: "+indexofobj
-    titledis.innerHTML="Title: "+objct.title
-    authdis.innerHTML="By: "+objct.author
-    pagesdis.innerHTML="N° of Pages: "+ objct.pages 
-    readdis.innerHTML="Read?: "+objct.read
-    delbutton.innerHTML="Delete Book"
-    
+  idobj.innerHTML="ID: "+objct.objindex
+  titledis.innerHTML="Title: "+objct.title
+  authdis.innerHTML="By: "+objct.author
+  pagesdis.innerHTML="N° of Pages: "+ objct.pages
+  if(objct.read==true){
+    readdis.innerHTML="Read?: Yes"
+  }else{
+    readdis.innerHTML="Read?: No"
+  }
+  delbutton.innerHTML="Delete Book"
+  chngbutton.innerHTML="Change  read"
+  card.appendChild(idobj);
+  card.appendChild(titledis);
+  card.appendChild(authdis);
+  card.appendChild(pagesdis);
+  card.appendChild(readdis);
+  card.appendChild(chngbutton);
+  card.appendChild(delbutton);
 
-    card.appendChild(idobj);
-    card.appendChild(titledis);
-    card.appendChild(authdis);
-    card.appendChild(pagesdis);
-    card.appendChild(readdis);
-    card.appendChild(delbutton);
-    contain.appendChild(card);
-
+  contain.appendChild(card);
 }
-function testfunc(inx){
+function deleteBook(inx){
   myLibrary.splice(inx, 1);
   console.log(myLibrary)
   render()
-
 }
-
-
+function changestate(obj){
+  console.log(obj)
+  if(obj.read==true){
+    obj.read=false
+    render()
+  }
+  else{
+    obj.read=true
+    render()
+  }
+}
